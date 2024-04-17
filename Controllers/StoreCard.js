0a542f2398002f578297e03e60c8aec87214e273
MUHANNAD SALKINI <mohanad.salkini@gmail.com>
2024-04-17 12:10:07 +0300

    Update payment options for StoreCard and payViaStoredCard controllers

--- Controllers/StoreCard.js
+++ Controllers/StoreCard.js
@@ -4,12 +4,12 @@ const storeCard = async (req, res) => {
   try {
     // Payment options
     const options = {
-      KK_Sahibi: "Test Test",
-      KK_No: "4155650100416",
-      KK_SK_Ay: "01",
-      KK_SK_Yil: "50",
-      KK_Kart_Adi: "MyFavCard",
-      KK_Islem_ID: "",
+      KK_Sahibi: "Test Test", // body
+      KK_No: "4155650100416", // body
+      KK_SK_Ay: "01", // body
+      KK_SK_Yil: "50", // body
+      KK_Kart_Adi: "MyFavCard", // body optional
+      KK_Islem_ID: "", // optional
     };
 
     // Initialize Parampos client
@@ -27,17 +27,17 @@ const payViaStoredCard = async (req, res) => {
   try {
     // Payment options
     const options = {
-      KS_GUID: "a27a99f2-c523-4caf-8bac-6f700a8b23eb",
-      CVV: "000",
-      KK_Sahibi_GSM: "5551231212",
-      Hata_URL: "https://dev.param.com.tr/en",
-      Basarili_URL: "https://dev.param.com.tr/tr",
-      Siparis_ID: "TKS_TahsilatS001",
-      Siparis_Aciklama: "string",
-      Taksit: "1",
-      Islem_Tutar: "100,00",
-      Toplam_Tutar: "100,00",
-      Islem_Guvenlik_Tip: "3D",
+      KS_GUID: "a27a99f2-c523-4caf-8bac-6f700a8b23eb", // params
+      CVV: "000", // body (3D islemler icin gerekli)
+      KK_Sahibi_GSM: "5551231212", // body
+      Hata_URL: "https://dev.param.com.tr/en", // env
+      Basarili_URL: "https://dev.param.com.tr/tr", // env
+      Siparis_ID: "TKS_TahsilatS001", // Unique order id
+      Siparis_Aciklama: "string", // body (zorunlu degil)
+      Taksit: "1", // body (1-12 taksit)
+      Islem_Tutar: "100,00", // body
+      Toplam_Tutar: "100,00", // body (Komisyon dahil)
+      Islem_Guvenlik_Tip: "3D", // body (NS (NonSecure) veya 3D)
       // Islem_ID: "string",
       IPAdr: "127.0.0.1",
       // Ref_URL: "string",
